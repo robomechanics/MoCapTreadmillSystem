6b3720617d156c8a4328092ba266233eff351aad
Barrett Werner <bwerner@andrew.cmu.edu>
2019-05-15 12:20:41 -0400

    added checksum

--- Misc/sendData_sync.m
+++ Misc/sendData_sync.m
@@ -3,17 +3,9 @@ function sendData_sync(tcpObj, cmdPacket)
 %flushinput(tcpObj);
 fprintf(tcpObj,'%c','RM');
 %fprintf(tcpObj, '%f', cmdPacket);
-cmdPacket = cast(cmdPacket,'single');
-fwrite(tcpObj, cmdPacket,'float');
-%checksum = cast(2 + length(cmdPacket)*4 + 2,'uint16');
-checksum = 0;
-for ii = 1:length(cmdPacket)
-    temp = de2bi(cmdPacket(ii))
-    checksum = checksum + de2bi(cmdPacket(ii))
-end
-checksum %= cast(checksum,'uint16')
-%fwrite(tcpObj, checksum,'uint16');
-% for ii = 1:length(cmdPacket)
-%     fprintf(tcpObj, '%d', cmdPacket(ii));
-%     fprintf(tcpObj,'%c',',');
-% end
+pack = cast(cmdPacket,'single');
+fwrite(tcpObj, pack,'float');
+
+binPack = typecast(pack, 'uint8');
+checksum = cast(sum(binPack),'uint16');
+fwrite(tcpObj, checksum,'uint16');
